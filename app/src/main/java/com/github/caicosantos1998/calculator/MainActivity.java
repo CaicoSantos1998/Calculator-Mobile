72b5901c3f9027b524c4eefe2a08364d44f6dd99
CS <caico_fc@hotmail.com>
2026-02-10 15:13:31 -0300

    The arithmetic logic for the subtract method was created

--- app/src/main/java/com/github/caicosantos1998/calculator/MainActivity.java
+++ app/src/main/java/com/github/caicosantos1998/calculator/MainActivity.java
@@ -62,7 +62,18 @@ public class MainActivity extends AppCompatActivity {
         double result = n1 + n2;
         resultTextView.setText(String.valueOf(result));
     }
-    private void subtract() {}
+    private void subtract() {
+        String n1S = number1EditText.getText().toString();
+        String n2S = number2EditText.getText().toString();
+        if(n1S.isEmpty() || n2S.isEmpty()) {
+            Toast.makeText(this, "Enter 2 numbers!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        double n1 = Double.parseDouble(n1S);
+        double n2 = Double.parseDouble(n2S);
+        double result = n1-n2;
+        resultTextView.setText(String.valueOf(result));
+    }
     private void multiply() {}
     private void divide() {}
 
